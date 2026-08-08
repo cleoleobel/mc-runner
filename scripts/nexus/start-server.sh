04d663aec1a2e32214f999a8b787e19341828252
cleoleobel <gokicraft19@gmail.com>
2026-08-08 15:51:44 -0400

    Final Optimization: 24/7 cron loop and memory stabilization with Aikar's Flags

--- scripts/nexus/start-server.sh
+++ scripts/nexus/start-server.sh
@@ -40,11 +40,24 @@ echo "[JVM] Configurando Heap dinámico: -Xms$XMS -Xmx$XMX"
 cat << EOF > user_jvm_args.txt
 -Xms${XMS}
 -Xmx${XMX}
--XX:+UseZGC
--XX:+ZProactive
+-XX:+UseG1GC
+-XX:+ParallelRefProcEnabled
+-XX:MaxGCPauseMillis=200
 -XX:+UnlockExperimentalVMOptions
--XX:+AlwaysPreTouch
 -XX:+DisableExplicitGC
+-XX:+AlwaysPreTouch
+-XX:G1NewSizePercent=30
+-XX:G1MaxNewSizePercent=40
+-XX:G1HeapRegionSize=8M
+-XX:G1ReservePercent=20
+-XX:G1HeapWastePercent=5
+-XX:G1MixedGCCountTarget=4
+-XX:InitiatingHeapOccupancyPercent=15
+-XX:G1MixedGCLiveThresholdPercent=90
+-XX:G1RSetUpdatingPauseTimePercent=5
+-XX:SurvivorRatio=32
+-XX:+PerfDisableSharedMem
+-XX:MaxTenuringThreshold=1
 EOF
 
 # 2. Aceptar EULA
